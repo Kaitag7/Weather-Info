3afd0038f665927f611ecb38574952bb52e8daef
Иса Ташбеков <i.tashbekov@citydrive.ru>
2024-02-05 12:49:19 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,52 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./style.css";
 
 export default function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=55a4a077c95a0fc476de3f4952afc4f7`;
+  const apiKey = process.env.REACT_APP_APIKEY;
 
-  const locationSearch = (event) => {
-    if (event.key === "Enter") {
-      fetch(url)
-        .then((res) => res.json())
-        .then((total) => setData(total));
-      setLocation("");
-    }
-  };
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
 
-  const temperature = (temp) => {
-    if (temp > 20) {
-      return "high";
+  const locationSearch = useCallback(
+    (event) => {
+      if (event.key === "Enter") {
+        fetch(url)
+          .then((res) => res.json())
+          .then((total) => setData(total));
+        setLocation("");
+      }
+    },
+    [url]
+  );
+
+  const backgroundImage = useCallback((temp) => {
+    switch (true) {
+      case temp > 20:
+        return "high";
+
+      case temp <= 20 && temp > 10:
+        return "medium";
+
+      case temp <= 10 && temp > 0:
+        return "low";
+
+      case temp <= 0:
+        return "cold";
+
+      default:
+        return "";
     }
-    if (temp < 20 && temp > 10) {
-      return "medium";
-    }
-    if (temp < 10 && temp > 0) {
-      return "low";
-    }
-    if (temp < 0) {
-      return "cold";
-    }
-    return "";
-  };
+  }, []);
 
   return (
-    <div className={`app ${temperature(data?.main?.temp)}`}>
+    <div className={`app ${backgroundImage(data?.main?.temp)}`}>
       <div className="search">
         <input
           value={location}
           onChange={(event) => setLocation(event.target.value)}
-          onKeyPress={locationSearch}
+          onKeyDown={locationSearch}
           type="text"
           placeholder="Enter Location"
         />
